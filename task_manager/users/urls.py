a77134d267be1629d31600d6b8981a8f1f867346
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-25 22:20:09 +0600

    Add simplejwt TokenObtainViews to urls

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .views import RegistrationAPIView
 
 urlpatterns = [
-    path('registration/', RegistrationAPIView.as_view(), name='registration')
+    path('registration/', RegistrationAPIView.as_view(), name='registration'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
