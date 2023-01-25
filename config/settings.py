9da11a9f048335def3bd2ddc19ccdd9c95e891f2
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-25 15:28:02 +0600

    Set users.User model as AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ LOCAL_APPS = [
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
