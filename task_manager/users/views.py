32da4aacf2b37cbe4b6ba878f024d7aae6e83a85
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-25 19:21:59 +0600

    Add RegistrationAPIView

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from drf_spectacular.utils import extend_schema, OpenApiResponse
+
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class RegistrationAPIView(GenericAPIView):
+
+    serializer_class = UserSerializer
+    authentication_classes = ()
+
+    # message in response
+    success_message = "Новый пользователь создан успешно"
+
+    @extend_schema(
+        responses={201: OpenApiResponse(description=success_message)}
+    )
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.create(serializer.validated_data)
+
+        return Response(self.success_message, status=status.HTTP_201_CREATED)
