8b2e66eb8f869718470e56eb8223cf27f31446e3
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-25 23:27:39 +0600

    Fix media files

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -33,3 +35,8 @@ urlpatterns = [
         name="swagger-ui",
     )
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
