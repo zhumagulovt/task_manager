15a81ab45c7a1d46fc2f699e6b14437d18637cd0
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-25 15:27:06 +0600

    Create custom user model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+
+        email = self.normalize_email(email)
+
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """
+    Custom user with unique email and username.
+    """
+
+    email = models.EmailField(
+        "email address",
+        unique=True,
+        error_messages={"unique": "This email is already in use"},
+    )
+
+    profile_picture = models.ImageField(
+        default="profile_pictures/default.jpg",
+        upload_to="profile_pictures/"
+    )
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
