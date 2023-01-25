2ca5732426faecd7a58e785b112e12b0517ffd6d
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-25 18:38:22 +0600

    Add useful fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+    return APIClient()
+
+
+@pytest.fixture
+def logged_in_client(api_client, user_factory):
+    user = user_factory.create()
+    api_client.force_authenticate(user=user)
+
+    return api_client, user
