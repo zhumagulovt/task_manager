b907edafe03b0dd34ae277d4562664d20d2da60a
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-25 19:21:40 +0600

    add UserSerializer

---
+++ task_manager/users/serializers.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
+
+from rest_framework import serializers
+from rest_framework.settings import api_settings
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    # make password field write only
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def validate(self, data):
+        """Validate password"""
+
+        user = User(**data)
+        password = data.get('password')
+
+        try:
+            validate_password(password, user)
+        except exceptions.ValidationError as e:
+            serializer_error = serializers.as_serializer_error(e)
+
+            raise serializers.ValidationError(
+                {"password": serializer_error[api_settings.NON_FIELD_ERRORS_KEY]}
+            )
+
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+
+        return user
