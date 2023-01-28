a3b2e41d41921b14e7755206ba696001f48d9e30
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-28 20:43:49 +0600

    Add search feature

--- task_manager/users/viewsets.py
+++ task_manager/users/viewsets.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import mixins, status
 
 from .permissions import IsAccountOwner
 from .serializers import UserSerializer
+from . import services
 
 User = get_user_model()
 
@@ -22,3 +25,19 @@ class UserViewSet(mixins.RetrieveModelMixin,
     queryset = User.objects.all()
     lookup_field = 'username'
     permission_classes = (IsAccountOwner, )
+
+    @action(methods=['GET'], detail=False)
+    def search(self, request):
+        """Search by username"""
+
+        q = request.query_params.get('q')
+
+        if q is not None and q != '':
+
+            qs = services.search_user_by_username(q)
+
+            data = self.get_serializer(qs, many=True)
+
+            return Response(data.data, status=status.HTTP_200_OK)
+
+        return Response([], status=status.HTTP_200_OK)
