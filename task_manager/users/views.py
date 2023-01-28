934afb00b6ab0d39775b0b9bf60d672f4c985f70
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-28 21:48:42 +0600

    Add ChangePasswordAPIView

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from drf_spectacular.utils import extend_schema, OpenApiResponse
 
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, ChangePasswordSerializer
+from . import services
 
 User = get_user_model()
 
@@ -28,3 +30,20 @@ class RegistrationAPIView(GenericAPIView):
         serializer.create(serializer.validated_data)
 
         return Response(self.success_message, status=status.HTTP_201_CREATED)
+
+
+class ChangePasswordAPIView(GenericAPIView):
+
+    permission_classes = (IsAuthenticated, )
+    serializer_class = ChangePasswordSerializer
+
+    def post(self, request):
+        user = request.user
+
+        serializer = self.get_serializer(data=request.data, context={'user': user})
+        serializer.is_valid(raise_exception=True)
+
+        new_password = serializer.validated_data.get('new_password')
+        services.change_user_password(user, new_password)
+
+        return Response('Пароль изменен', status=status.HTTP_200_OK)
