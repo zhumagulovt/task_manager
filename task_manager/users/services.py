78ea875c04079783aa12099844846e189bfb070e
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-28 21:48:23 +0600

    Add function for changing password

--- task_manager/users/services.py
+++ task_manager/users/services.py
@@ -6,3 +6,8 @@ from .models import User
 def search_user_by_username(username: str) -> QuerySet:
 
     return User.objects.filter(username__icontains=username)
+
+
+def change_user_password(user: User, password: str) -> None:
+    user.set_password(password)
+    user.save(update_fields=['password'])
