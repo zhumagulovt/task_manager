d99281d179cd990eb956b3087d3d1871898f1c55
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-28 18:32:47 +0600

    Add UserViewSet to urls

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,14 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import SimpleRouter
 
 from .views import RegistrationAPIView
+from .viewsets import UserViewSet
+
+router = SimpleRouter()
+router.register('', UserViewSet, 'user')
 
 urlpatterns = [
     path('registration/', RegistrationAPIView.as_view(), name='registration'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls))
 ]
