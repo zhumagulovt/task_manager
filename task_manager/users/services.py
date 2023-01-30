608d1d0ac0fdfd49ef5267e2fa15a46d579fc37f
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-30 20:37:13 +0600

    Add get_projects_of_user and get_user_by_username functions

--- task_manager/users/services.py
+++ task_manager/users/services.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from django.db.models.query import QuerySet
 
 from .models import User
@@ -8,6 +10,15 @@ def search_user_by_username(username: str) -> QuerySet:
     return User.objects.filter(username__icontains=username)
 
 
+def get_user_by_username(username: str) -> User:
+    user = get_object_or_404(User.objects.all(), username=username)
+    return user
+
+
 def change_user_password(user: User, password: str) -> None:
     user.set_password(password)
     user.save(update_fields=['password'])
+
+
+def get_projects_of_user(user: User) -> QuerySet:
+    return user.projects.all()
