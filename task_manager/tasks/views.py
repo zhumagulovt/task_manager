f8979044a420f3f91bf44fe80971f5bf3e69e6d0
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-30 18:22:45 +0600

    Add view for creating new project

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import ProjectSerializer
+from . import services
+
+
+class ProjectCreateAPIView(CreateAPIView):
+    """Create new project"""
+
+    serializer_class = ProjectSerializer
+    permission_classes = (IsAuthenticated, )
+    queryset = services.get_all_projects()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
