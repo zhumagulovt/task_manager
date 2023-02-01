8c5c1061a475a43b7efeeb80870fc26b9ab79111
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-02-01 22:54:29 +0600

    Add Task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+from task_manager.projects.models import BaseModel, Project
+
+User = get_user_model()
+
+
+class Task(BaseModel):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    completed = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
+    completed_at = models.DateTimeField(blank=True, null=True)
+    performer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks'
+    )
