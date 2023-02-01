c94777d2d891a8f58db1046b2c1720cd0f2aa8f3
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-02-01 22:46:41 +0600

    startapp tasks

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,8 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "task_manager.users",
-    "task_manager.projects"
+    "task_manager.projects",
+    "task_manager.tasks"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
